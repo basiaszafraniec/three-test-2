f4c3de8915db5c17683fca5c21646e2b389ea061
basiaszafraniec <basia.szafraniec@gmail.com>
2024-06-05 14:46:01 +0200

    installed stuff

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 import * as THREE from 'three';
+
+const canvas = document.querySelector("#canvas");
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+const camera = new THREE.OrthographicCamera();
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+renderer.render(scene,camera);
